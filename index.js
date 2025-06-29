89c1dc5a2dd1ac6cfef238b7dbc46528a6c94240
Mr-Dan-kibet <dan.rotich@student.moringaschool.com>
2025-06-29 12:17:39 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+// index.js
+
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  // Export it
+  module.exports = { superbowlWin };
+  
